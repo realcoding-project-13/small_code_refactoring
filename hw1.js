a91558f8bb537d9501e921957b7b78e6f905a221
Jeong Tae Yong <wjdxodyd11@gmail.com>
2019-05-13 21:18:26 +0900

    rename

--- hw1.js
+++ hw1.js
@@ -1,11 +1,11 @@
 let today = new Date();
 let firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
 let lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-let tData = document.getElementsByClassName('tData');
+let tableDataSet = document.getElementsByClassName('tData');
 let yearmonth = document.getElementById('yearmonth');
 let startCal = firstDate.getDay();
-let addmodal = document.getElementById('addModal');
-let chgmodal = document.getElementById('chgModal');
+let addModal = document.getElementById('addModal');
+let changeModal = document.getElementById('chgModal');
 
 function init() {
     yearmonth.innerHTML = today.getFullYear() + '年 ' + (today.getMonth() + 1) + '月'; // 년월 출력
@@ -18,43 +18,43 @@ function init() {
 }
 function setDateToCalendar(index){
     let date = index - startCal + 1;
-    tData[index].innerHTML = date;
-    tData[index].classList.add('tData');
+    tableDataSet[index].innerHTML = date;
+    tableDataSet[index].classList.add('tableDataSet');
 }
 function setColorToDay(index){
     if (index === today.getDate() + startCal - 1 ) { // 현재
-        tData[index].style.backgroundColor = '#96e3ff';
-        tData[index].setAttribute('ondblclick', 'addPopup(' + i + ')');
+        tableDataSet[index].style.backgroundColor = '#96e3ff';
+        tableDataSet[index].setAttribute('ondblclick', 'addPopup(' + i + ')');
     } else if (index < today.getDate() + startCal - 1 ) { // 지난 날
-        tData[index].style.backgroundColor = '#e3e4ea';
+        tableDataSet[index].style.backgroundColor = '#e3e4ea';
     } else { // 남은 날
-        tData[index].style.backgroundColor = '#d9e8ce';
-        tData[index].setAttribute('ondblclick', 'addPopup(' + i + ')');
+        tableDataSet[index].style.backgroundColor = '#d9e8ce';
+        tableDataSet[index].setAttribute('ondblclick', 'addPopup(' + i + ')');
     }
 }
 
 function addPopup(tdNum) {
-    addmodal.getElementsByTagName('p')[0].innerHTML = (tdNum - startCal + 1) + '日 일정 추가';
-    addmodal.getElementsByTagName('input')[0].value = '';
-    addmodal.getElementsByTagName('input')[1].setAttribute('onclick', 'addList(' + tdNum + ')');
-    addmodal.style.display = "block";
+    addModal.getElementsByTagName('p')[0].innerHTML = (tdNum - startCal + 1) + '日 일정 추가';
+    addModal.getElementsByTagName('input')[0].value = '';
+    addModal.getElementsByTagName('input')[1].setAttribute('onclick', 'addList(' + tdNum + ')');
+    addModal.style.display = "block";
 }
 
 function addList(tdNum) {
-  let div = document.createElement('div');
+  let divElement = document.createElement('div');
   let schedule = addList_newSchedule();
 
   if(schedule){
     let btn = addList_button(tdNum);
-    div.appendChild(schedule);
-    div.appendChild(btn);
-    tData[tdNum].appendChild(div);
+    divElement.appendChild(schedule);
+    divElement.appendChild(btn);
+    tableDataSet[tdNum].appendChild(divElement);
   }
   closeAddBox();
 }
 
 function addList_newSchedule() {
-  let inputTxt = addmodal.getElementsByTagName('input')[0].value;
+  let inputTxt = addModal.getElementsByTagName('input')[0].value;
   if (inputTxt !== '') {
     let p = document.createElement('input');
     p.value = inputTxt;
@@ -65,7 +65,7 @@ function addList_newSchedule() {
 
 function addList_button(tdNum) {
   let btn = document.createElement('button');
-  btn.setAttribute('onclick', 'changePopup(' + tdNum + ',' + tData[tdNum].getElementsByTagName('div').length + ')');
+  btn.setAttribute('onclick', 'changePopup(' + tdNum + ',' + tableDataSet[tdNum].getElementsByTagName('div').length + ')');
   btn.classList.add('changeBtn');
   btn.innerHTML = '&times';
   return btn;
@@ -77,28 +77,28 @@ function changePopup(tdNum, listNum) {
 }
 
 function setChangePopupInputButton(tdNum, listNum) {
-  let numberOfThisDaySchedule = tData[tdNum].getElementsByTagName('div').length;
+  let numberOfThisDaySchedule = tableDataSet[tdNum].getElementsByTagName('div').length;
   let originalDate = [Math.floor((tdNum - startCal + 1) / 10), (tdNum - startCal + 1) % 10];
   let fullDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + originalDate[0] + originalDate[1];
   alert(fullDate);
-  let selectDateToChangeButton = chgmodal.getElementsByTagName('input')[0];
-  let selectOrderToChangeButton = chgmodal.getElementsByTagName('input')[1];
+  let selectDateToChangeButton = changeModal.getElementsByTagName('input')[0];
+  let selectOrderToChangeButton = changeModal.getElementsByTagName('input')[1];
   selectOrderToChangeButton.removeAttribute('disabled');
 
   selectDateToChangeButton.value = fullDate; // 오늘을 나타내는 값을 날짜 변경 초기 값으로 설정
   selectOrderToChangeButton.value = (listNum + 1); // 해당 날짜의 클릭한 일정의 인덱스 값이 listNum이므로 +1을 해주어 보기 쉽게함.
   selectOrderToChangeButton.setAttribute('max', numberOfThisDaySchedule); // 해당 날짜의 일정 개수를 선택가능한 max값으로 설정해줌.
 
-  chgmodal.getElementsByTagName('input')[2].setAttribute('onclick', 'chgList(' + tdNum + ',' + listNum + ')'); // Save 버튼을 누른 경우(일정 순서 변경)
-  chgmodal.getElementsByTagName('input')[3].setAttribute('onclick', 'dltList(' + tdNum + ',' + listNum + ')'); // Delete 버튼을 누른 경우(일정 삭제)
+  changeModal.getElementsByTagName('input')[2].setAttribute('onclick', 'chgList(' + tdNum + ',' + listNum + ')'); // Save 버튼을 누른 경우(일정 순서 변경)
+  changeModal.getElementsByTagName('input')[3].setAttribute('onclick', 'deleteList(' + tdNum + ',' + listNum + ')'); // Delete 버튼을 누른 경우(일정 삭제)
 }
 
 function setChangePopupButtonClickEvent() {
   let fullDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDay();
-  let selectDateToChangeButton = chgmodal.getElementsByTagName('input')[0];
-  let selectOrderToChangeButton = chgmodal.getElementsByTagName('input')[1];
+  let selectDateToChangeButton = changeModal.getElementsByTagName('input')[0];
+  let selectOrderToChangeButton = changeModal.getElementsByTagName('input')[1];
 
-  chgmodal.style.display = "block";
+  changeModal.style.display = "block";
 
   window.onchange = function (event) {
       if (event.target === selectDateToChangeButton) {
@@ -113,8 +113,8 @@ function setChangePopupButtonClickEvent() {
 }
 
 function chgList(tdNum, listNum) {
-    let chgDBtn = chgmodal.getElementsByTagName('input')[0];
-    let divObj = tData[tdNum].getElementsByTagName('div')[listNum];
+    let chgDBtn = changeModal.getElementsByTagName('input')[0];
+    let divObj = tableDataSet[tdNum].getElementsByTagName('div')[listNum];
     let chgNum = (document.getElementById('chgModal').getElementsByTagName('input')[1].value - 1);
     let splitDate = chgDBtn.value.split('-');
 
@@ -129,7 +129,7 @@ function chgList(tdNum, listNum) {
           chgListDate(splitDate[2] * 1 + startCal - 1, divObj);
 
       else if (listNum !== chgNum)
-          chgListNum(tdNum, listNum, chgNum, divObj);
+          changeListNum(tdNum, listNum, chgNum, divObj);
 
       reNumList(tdNum);
     }
@@ -141,45 +141,45 @@ function chgList(tdNum, listNum) {
 }
 
 function reNumList(tdNum){
-  for(let i = 0; i<tData[tdNum].getElementsByTagName('div').length;i++){
-      tData[tdNum].getElementsByTagName('div')[i].getElementsByTagName('button')[0].setAttribute('onclick', 'changePopup(' + tdNum + ',' + i + ')');
+  for(let i = 0; i<tableDataSet[tdNum].getElementsByTagName('div').length; i++){
+      tableDataSet[tdNum].getElementsByTagName('div')[i].getElementsByTagName('button')[0].setAttribute('onclick', 'changePopup(' + tdNum + ',' + i + ')');
   }
 }
 
 function chgListDate(tdNum, divObj){
-    let lastNum = tData[tdNum].getElementsByTagName('div').length - 1;
+    let lastNum = tableDataSet[tdNum].getElementsByTagName('div').length - 1;
 
     if (lastNum === -1) {
-        tData[tdNum].appendChild(divObj);
+        tableDataSet[tdNum].appendChild(divObj);
         divObj.getElementsByTagName('button')[0].setAttribute('onclick', 'changePopup(' + tdNum + ',0)');
     }
     else {
-        tData[tdNum].getElementsByTagName('div')[lastNum].after(divObj);
+        tableDataSet[tdNum].getElementsByTagName('div')[lastNum].after(divObj);
         divObj.getElementsByTagName('button')[0].setAttribute('onclick', 'changePopup(' + tdNum + ',' + (lastNum+1) + ')');
     }
 
 
 }
 
-function chgListNum(tdNum, listNum, newNum, divObj) {
+function changeListNum(tdNum, listNum, newNum, divObj) {
     if (newNum === 0) {
-        tData[tdNum].getElementsByTagName('div')[0].before(divObj);
+        tableDataSet[tdNum].getElementsByTagName('div')[0].before(divObj);
     }
 
     else if(newNum>listNum){
-        tData[tdNum].getElementsByTagName('div')[newNum].after(divObj);
+        tableDataSet[tdNum].getElementsByTagName('div')[newNum].after(divObj);
     }
 
     else {
-        tData[tdNum].getElementsByTagName('div')[newNum - 1].after(divObj);
+        tableDataSet[tdNum].getElementsByTagName('div')[newNum - 1].after(divObj);
     }
 }
 
-function dltList(tdNum, listNum) {
-    tData[tdNum].removeChild(tData[tdNum].getElementsByTagName('div')[listNum]);
+function deleteList(tdNum, listNum) {
+    tableDataSet[tdNum].removeChild(tableDataSet[tdNum].getElementsByTagName('div')[listNum]);
     document.getElementById('chgModal').style.display = "none";
-    for(let i = 0; i<tData[tdNum].getElementsByTagName('div').length;i++){
-        tData[tdNum].getElementsByTagName('div')[i].getElementsByTagName('button')[0].setAttribute('onclick', 'changePopup(' + tdNum + ',' + i + ')');
+    for(let i = 0; i<tableDataSet[tdNum].getElementsByTagName('div').length; i++){
+        tableDataSet[tdNum].getElementsByTagName('div')[i].getElementsByTagName('button')[0].setAttribute('onclick', 'changePopup(' + tdNum + ',' + i + ')');
     }
 }
 
